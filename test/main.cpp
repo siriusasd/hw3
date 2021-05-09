49dbedfa9084731ff324a4b85e615ba87f96cdae
siriusasd <shawn1.2311@gmail.com>
2021-05-10 05:03:21 +0800

    update

--- test/main.cpp
+++ test/main.cpp
@@ -100,7 +100,17 @@ void choose_function()
 void choose_angle()
 {
     myled3=1;
-    printf("num= %d",angle_index);
+    uLCD.cls();
+    uLCD.printf("\ngesture UI:\n");
+    uLCD.printf("\nselect threshold angle\n");
+    if(angle_index==1)        
+        uLCD.printf("\n30 is selected\n");
+    else if(angle_index==2)
+        uLCD.printf("\n45 is selected\n");
+    else if(angle_index==3)
+        uLCD.printf("\n60 is selected\n");
+    else if(angle_index==4)
+        uLCD.printf("\n90 is selected\n");
 }
 
 void dct()
@@ -274,6 +284,7 @@ void dct()
 
 int main(int argc, char* argv[])
 {
+    uLCD.cls();
     uLCD.printf("\nSelection:\n"); //Default Green on black text
     uLCD.printf("\ngesture UI\n");
     uLCD.printf("\ntilt angle detection\n");
@@ -309,163 +320,6 @@ void gesture (Arguments *in, Reply *out)   {
     uLCD.printf("\n90\n");
 
     t2.start(dct);
-/*    
-    // Whether we should clear the buffer next time we fetch data
-    bool should_clear_buffer = false;
-    bool got_data = false;
-
-    // The gesture index of the prediction
-    int gesture_index;
-
-    // Set up logging.
-    static tflite::MicroErrorReporter micro_error_reporter;
-    tflite::ErrorReporter* error_reporter = &micro_error_reporter;
-
-    // Map the model into a usable data structure. This doesn't involve any
-    // copying or parsing, it's a very lightweight operation.
-    const tflite::Model* model = tflite::GetModel(g_magic_wand_model_data);
-    if (model->version() != TFLITE_SCHEMA_VERSION) {
-        error_reporter->Report(
-            "Model provided is schema version %d not equal "
-            "to supported version %d.",
-            model->version(), TFLITE_SCHEMA_VERSION);
-        return -1;
-    }
-
-    // Pull in only the operation implementations we need.
-    // This relies on a complete list of all the ops needed by this graph.
-    // An easier approach is to just use the AllOpsResolver, but this will
-    // incur some penalty in code space for op implementations that are not
-    // needed by this graph.
-    static tflite::MicroOpResolver<6> micro_op_resolver;
-    micro_op_resolver.AddBuiltin(
-        tflite::BuiltinOperator_DEPTHWISE_CONV_2D,
-        tflite::ops::micro::Register_DEPTHWISE_CONV_2D());
-    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_MAX_POOL_2D,
-                                tflite::ops::micro::Register_MAX_POOL_2D());
-    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_CONV_2D,
-                                tflite::ops::micro::Register_CONV_2D());
-    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_FULLY_CONNECTED,
-                                tflite::ops::micro::Register_FULLY_CONNECTED());
-    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_SOFTMAX,
-                                tflite::ops::micro::Register_SOFTMAX());
-    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_RESHAPE,
-                                tflite::ops::micro::Register_RESHAPE(), 1);
-
-    // Build an interpreter to run the model with
-    static tflite::MicroInterpreter static_interpreter(
-        model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
-    tflite::MicroInterpreter* interpreter = &static_interpreter;
-
-    // Allocate memory from the tensor_arena for the model's tensors
-    interpreter->AllocateTensors();
-
-    // Obtain pointer to the model's input tensor
-    TfLiteTensor* model_input = interpreter->input(0);
-    if ((model_input->dims->size != 4) || (model_input->dims->data[0] != 1) ||
-        (model_input->dims->data[1] != config.seq_length) ||
-        (model_input->dims->data[2] != kChannelNumber) ||
-        (model_input->type != kTfLiteFloat32)) {
-        error_reporter->Report("Bad input tensor parameters in model");
-        return -1;
-    }
-
-    int input_length = model_input->bytes / sizeof(float);
-
-    TfLiteStatus setup_status = SetupAccelerometer(error_reporter);
-    if (setup_status != kTfLiteOk) {
-        error_reporter->Report("Set up failed\n");
-        return -1;
-    }
-
-    //error_reporter->Report("Set up successful...\n");
-
-    while (true) {
-
-        // Attempt to read new data from the accelerometer
-        got_data = ReadAccelerometer(error_reporter, model_input->data.f,
-                                    input_length, should_clear_buffer);
-
-        // If there was no new data,
-        // don't try to clear the buffer again and wait until next time
-        if (!got_data) {
-        should_clear_buffer = false;
-        continue;
-        }
-
-        // Run inference, and report any error
-        TfLiteStatus invoke_status = interpreter->Invoke();
-        if (invoke_status != kTfLiteOk) {
-        error_reporter->Report("Invoke failed on index: %d\n", begin_index);
-        continue;
-        }
-
-        // Analyze the results to obtain a prediction
-        gesture_index = PredictGesture(interpreter->output(0)->data.f);
-        
-        
-        // Clear the buffer next time we read data
-        should_clear_buffer = gesture_index < label_num;
-
-        memset(bufff, 0, 256);
-        
-        
-        // Produce an output
-        if (gesture_index < label_num) { 
-            //myled3=1;
-            if(angle_index<4)
-                angle_index+=1;
-            else
-                angle_index=1;
-            strcpy(bufff,config.output_message[gesture_index]);
-            if(angle_index==1)
-            {
-                uLCD.cls();
-                uLCD.printf("\ngesture UI:\n");
-                uLCD.printf("\nselect threshold angle\n");
-                uLCD.printf("\n30 <--\n");
-                uLCD.printf("\n45\n");
-                uLCD.printf("\n60\n");
-                uLCD.printf("\n90\n");
-                sw3.rise(choose_angle);
-            }
-            else if(angle_index==2)
-            {
-                uLCD.cls();
-                uLCD.printf("\ngesture UI:\n");
-                uLCD.printf("\nselect threshold angle\n");
-                uLCD.printf("\n30\n");
-                uLCD.printf("\n45 <--\n");
-                uLCD.printf("\n60\n");
-                uLCD.printf("\n90\n");
-                sw3.rise(choose_angle);
-            }
-            else if(angle_index==3)
-            {
-                uLCD.cls();
-                uLCD.printf("\ngesture UI:\n");
-                uLCD.printf("\nselect threshold angle\n");
-                uLCD.printf("\n30\n");
-                uLCD.printf("\n45\n");
-                uLCD.printf("\n60 <--\n");
-                uLCD.printf("\n90\n");
-                sw3.rise(choose_angle);
-            }
-            else if(angle_index==4)
-            {
-                uLCD.cls();
-                uLCD.printf("\ngesture UI:\n");
-                uLCD.printf("\nselect threshold angle\n");
-                uLCD.printf("\n30\n");
-                uLCD.printf("\n45\n");
-                uLCD.printf("\n60\n");
-                uLCD.printf("\n90 <--\n");
-                sw3.rise(choose_angle);
-            }
-            //error_reporter->Report(config.output_message[gesture_index]);
-        }
-    }
-    */
 }
 void tilt (Arguments *in, Reply *out)   {
     bool success = true;
